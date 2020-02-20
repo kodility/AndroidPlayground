bb7ffe6417e82050225615d820aeddc33a4518e5
Mostafa Monowar <monowar1993@gmail.com>
2020-02-20 22:49:08 +0100

    merge theme and lib-base

--- lib-base/src/main/java/com/monowar/libbase/utils/Utils.kt
+++ lib-base/src/main/java/com/monowar/libbase/utils/Utils.kt
@@ -1,12 +1,15 @@
 package com.monowar.libbase.utils
 
 import android.content.Context
+import android.content.res.Configuration
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkInfo
 import android.os.Build
-
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+// *** Network *** \\
 fun isNetworkAvailable(context: Context): Boolean {
     val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -18,4 +21,15 @@ fun isNetworkAvailable(context: Context): Boolean {
         val activeNetworkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
         activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
+}
+
+// *** Theme *** \\
+fun isDarkMode(context: Context): Boolean {
+    return context.resources.configuration.uiMode and
+            Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+}
+
+// *** Resource *** \\
+fun getColorHexString(context: Context, @ColorRes resId: Int): String {
+    return "#" + Integer.toHexString(ContextCompat.getColor(context, resId))
 }
